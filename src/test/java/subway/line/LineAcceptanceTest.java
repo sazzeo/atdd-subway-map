5f3e2da3b73c75bd74f22bb1a9a45be350fccf94
ji-younglim <imjee02@gmail.com>
2024-07-06 13:07:41 +0900

    test(step2): line 테스트 코드 응답 결과를 반환받도록 수정

--- src/test/java/subway/line/LineAcceptanceTest.java
+++ src/test/java/subway/line/LineAcceptanceTest.java
@@ -1,11 +1,6 @@
 package subway.line;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-
 import io.restassured.RestAssured;
-import java.util.List;
-import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -13,54 +8,47 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 @DisplayName("노선 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class LineAcceptanceTest {
 
-  private static final String URL_PREFIX = "/lines";
+    private static final String URL_PREFIX = "/lines";
 
-  @Nested
-  class WhenNew {
+    @Nested
+    class WhenNew {
 
-    @DisplayName("노선 등록시 노선명,노선색,상행역,하행역,거리를 함께 등록한다")
-    @Test
-    void createLine() {
-      // Given 노선을 생성하면
-      var lineFixture = LineFixture.create();
+        @DisplayName("노선 등록시 노선명,노선색,상행역,하행역,거리를 함께 등록하고 생성된 결과를 응답받는다.")
+        @Test
+        void createLine() {
+            // Given 노선을 생성하면
+            var lineFixture = LineFixture.create();
 
-      // Then 신규 노선이 생성된다.
-      var createdExtractableResponse = RestAssured.given().log().all()
-          .contentType(MediaType.APPLICATION_JSON_VALUE)
-          .basePath(URL_PREFIX)
-          .body(lineFixture)
-          .when().post()
-          .then().log().all()
-          .statusCode(HttpStatus.CREATED.value())
-          .extract();
+            // Then 신규 노선이 생성된다.
+            var extractableResponse = RestAssured.given().log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .basePath(URL_PREFIX)
+                    .body(lineFixture)
+                    .when().post()
+                    .then().log().all()
+                    .statusCode(HttpStatus.CREATED.value())
+                    .extract();
 
-      assertThat(createdExtractableResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+            assertThat(extractableResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+            var jsonPath = extractableResponse.response().jsonPath();
 
-//      // Then 신규 생성된 노선이 조회된다.
-//      var createdUrl = createdExtractableResponse.header("location");
-//      var lineJsonPath = RestAssured.given().log().all()
-//          .contentType(MediaType.APPLICATION_JSON_VALUE)
-//          .body(params)
-//          .when().get(createdUrl)
-//          .then().log().all()
-//          .statusCode(HttpStatus.OK.value())
-//          .extract().response().jsonPath();
-//
-//      var name = lineJsonPath.getString("name");
-//      var color = lineJsonPath.getString("color");
-//      List<Long> stations = lineJsonPath.getList("$.stations[*].id");
-//
-//      assertAll(() -> {
-//            assertThat(name).isEqualTo("2호선");
-//            assertThat(color).isEqualTo("bg-green-600");
-//            assertThat(stations).containsAnyOf(1L, 2L);
-//          }
-//      );
+            //Then 생성된 노선을 응답받는다.
+            assertAll(() -> {
+                assertThat(jsonPath.getString("color")).isEqualTo(lineFixture.get("color"));
+                assertThat(jsonPath.getString("name")).isEqualTo(lineFixture.get("name"));
+                assertThat(jsonPath.getList("stations.id")).containsAnyOf(lineFixture.get("upStationId"), lineFixture.get("downStationId"));
+            });
+
+        }
+
+
+
     }
-
-  }
 }
