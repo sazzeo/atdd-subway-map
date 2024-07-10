5e3cac4e97985a2bbbc59801112493e3e6e6df7c
jiyeong-dt <imjee02@gmail.com>
2024-07-10 23:38:54 +0900

    feat(step3): section 엔티티 추가

---
+++ src/main/java/subway/line/domain/Section.java
@@ -0,0 +1,47 @@
+package subway.line.domain;
+
+import subway.station.Station;
+
+import javax.persistence.*;
+
+@Entity
+public class Section {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Station upStation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Station downStation;
+
+    private Long distance;
+
+    public Section() {
+
+    }
+
+    public Section(final Station upStation, final Station downStation, final Long distance) {
+        this.upStation = upStation;
+        this.downStation = downStation;
+        this.distance = distance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+
+    public Long getDistance() {
+        return distance;
+    }
+}
