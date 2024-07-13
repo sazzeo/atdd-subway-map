4b3d0044ae15d0d3a57423949bf2077bf0449076
ji-younglim <imjee02@gmail.com>
2024-07-13 14:37:30 +0900

    refactor: test 코드 리팩토링

--- src/test/java/subway/line/domain/LineTest.java
+++ src/test/java/subway/line/domain/LineTest.java
@@ -1,5 +1,6 @@
 package subway.line.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import subway.line.exception.InsufficientStationsException;
@@ -12,14 +13,20 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LineTest {
 
+    private Line line;
+    private Long 상행역;
+    private Long 하행역;
+
+    @BeforeEach
+    void setUp() {
+        상행역 = 1L;
+        하행역 = 2L;
+        line = new Line("2호선", "green", new Section(상행역, 하행역, 10L));
+    }
+
     @DisplayName("새로 등록하려는 상행역이 기존 하행역이 아니면 에러를 발생시킨다")
     @Test
     void test1() {
-        //given 기존역에
-        var 상행역 = 1L;
-        var 하행역 = 2L;
-        Line line = new Line("2호선", "green", new Section(상행역, 하행역, 10L));
-
         //when 새로 등록하려는 상행역이 기존 하행역이 아니면
         var 새상행역 = 3L;
         var 새하행역 = 4L;
@@ -33,11 +40,6 @@ class LineTest {
     @DisplayName("새로 등록하려는 하행역이 이미 등록된 경우 에러를 발생시킨다")
     @Test
     void test2() {
-        //given 기존역에
-        var 상행역 = 1L;
-        var 하행역 = 2L;
-        Line line = new Line("2호선", "green", new Section(상행역, 하행역, 10L));
-
         //when 새로 등록하려는 하행역이 이미 등록된 경우
         var 새상행역 = 2L;
         var 새하행역 = 1L;
@@ -51,18 +53,13 @@ class LineTest {
     @DisplayName("역이 3개 이상일 때 삭제하려는 역이 기존 종착역이 아닌 경우 에러를 발생시킨다")
     @Test
     void test3() {
-        //역이 3개 이상일때
-        var 역1 = 1L;
-        var 역2 = 2L;
         var 역3 = 3L;
+        line.addSection(하행역, 역3, 10L);
 
-        Line line = new Line("2호선", "green", new Section(역1, 역2, 10L));
-        line.addSection(역2, 역3, 10L);
-
+        //when 삭제하려는 역이 현재 종착역이 아닌 경우
         //then 에러를 발생시킨다
         assertThrows(NotTerminusStationException.class, () ->
-                //when 삭제하려는 역이 현재 종착역이 아닌 경우
-                line.removeLastStation(역2)
+                line.removeLastStation(하행역)
         );
     }
 
@@ -70,14 +67,10 @@ class LineTest {
     @Test
     void test4() {
         //역이 2개 이하로 존재하는 경우
-        var 역1 = 1L;
-        var 역2 = 2L;
-        Line line = new Line("2호선", "green", new Section(역1, 역2, 10L));
-
+        //when 하행역 삭제시
         //then 에러를 발생시킨다
         assertThrows(InsufficientStationsException.class, () ->
-                //when 하행역 삭제시
-                line.removeLastStation(역2)
+                line.removeLastStation(하행역)
         );
     }
 
@@ -85,19 +78,14 @@ class LineTest {
     @Test
     void test5() {
         //역이 3개 이상일때
-        var 역1 = 1L;
-        var 역2 = 2L;
         var 역3 = 3L;
-
-        Line line = new Line("2호선", "green", new Section(역1, 역2, 10L));
-        line.addSection(역2, 역3, 10L);
-
+        line.addSection(하행역, 역3, 10L);
 
         //마지막역을 삭제하면
         line.removeLastStation(역3);
 
         //then 조회되지 않는다
-        assertThat(line.getStationIds()).containsExactly(역1, 역2);
+        assertThat(line.getStationIds()).containsExactly(상행역, 하행역);
     }
 
 }
